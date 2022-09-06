14031f4d9fd034df5994e902f3ff4fbf760ace63
Nihal <nihal@nevercode.io>
2022-09-06 13:47:09 +0300

    fixing integration tests

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:firebase_test_lab/main.dart';
+import 'package:test_lab_ios/main.dart';
 // import 'dart:io';
 
 void main() {
